97c1baf179aec481586655d2aee6e313470b7ac7
IPPK93 <61944163+IPPK93@users.noreply.github.com>
2022-05-10 00:41:20 +0300

    Make Gector work with complex operations

--- Autograd/Tests.h
+++ Autograd/Tests.h
@@ -49,7 +49,6 @@ void test_add()
 		auto r1 = g1 + g2;
 		assert(r1 == Gector<double>({ 3., 3. }));
 		r1.backward(Gector<double> {-1., 1.});
-		std::cout << g1.get_grad();
 		assert(g1.get_grad() == Gector<double>({ -1., 1. }));
 		assert(g2.get_grad() == Gector<double>({ -1., 1. }));
 	}
@@ -66,27 +65,46 @@ void test_mul()
 	assert(g2.get_grad() == Gector<double>({ -1., 2., 6. }));
 }
 
+void test_complex()
+{
+	Gector a{ 2., -2. };
+	Gector b{ 5., 10. };
+
+	auto c = a * b + (a + b) + a * a;
+
+	auto d = c + a + b;
+
+	d.backward({ 1., 1. });
+
+	assert(c.get_grad() == Gector<double>({1., 1.}));
+	auto a_copy = Gector<double>(2. * a.data);
+	auto b_copy = Gector<double>(b.data);
+	auto unit = Gector<double>(std::vector<double>(a.size(), 1));
+	unit = unit + unit;
+	assert(a.get_grad() == b_copy + unit + a_copy);
+}
+
 void minimize()
 {
-	Gector<double> x_{ 2., -2.};
-	
-	for (auto i = 0; i < 100; ++i)
-	{
-		Gector<double> x = x_;
-		std::cout << "x = " << x.data;
-		auto square = x * x;
-		std::cout << "x ** 2 = " << square.data;
-		auto sum_of_squares = square.sum();
-		std::cout << "sum(x**2) = " << sum_of_squares.data;
-		sum_of_squares.backward();
-		std::cout << "grad(x**2) = " << x.get_grad();
-		auto alpha = 0.1;
-		auto delta_x = x.get_grad() * alpha;
-		Gector<double> y = x - delta_x;
-		x = y;
-		x_.data = x.data;
-		std::cout << i  << ": "  << y.data << "\n\n";
-	}
+	//Gector<double> x_{ 2., -2.};
+	//
+	//for (auto i = 0; i < 100; ++i)
+	//{
+	//	Gector<double> x = x_;
+	//	std::cout << "x = " << x.data;
+	//	auto square = x * x;
+	//	std::cout << "x ** 2 = " << square.data;
+	//	auto sum_of_squares = square.sum();
+	//	std::cout << "sum(x**2) = " << sum_of_squares.data;
+	//	sum_of_squares.backward();
+	//	std::cout << "grad(x**2) = " << x.get_grad();
+	//	auto alpha = 0.1;
+	//	auto delta_x = Gector<double>(x.get_grad(), false) * alpha;
+	//	Gector<double> y = x - delta_x;
+	//	x = y;
+	//	x_.data = x.data;
+	//	std::cout << i  << ": "  << y.data << "\n\n";
+	//}
 }
 
 void test_all()
@@ -94,6 +112,7 @@ void test_all()
 	test_sum();
 	test_add();
 	test_mul();
-	minimize();
+	test_complex();
+	//minimize();
 }
 
